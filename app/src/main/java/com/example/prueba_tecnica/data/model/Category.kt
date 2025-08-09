aa5df539e091d82ea73da1f96a9e6c68bcd4bbeb
luisalealr <luisafernandalero@gmail.com>
2025-08-09 12:05:17 -0500

    Init: First commit

---
+++ app/src/main/java/com/example/prueba_tecnica/data/model/Category.kt
@@ -0,0 +1,18 @@
+package com.example.prueba_tecnica.data.model
+
+data class Category(
+    val category: String,
+    val categoryId: Int,
+    val createTime: String,
+    val deleteTime: Any,
+    val id: Int,
+    val priority: String,
+    val priorityId: Int,
+    val reportFolioId: Int,
+    val requestText: Boolean,
+    val subcategory: String,
+    val subcategoryId: Int,
+    val subcategoryText: Any,
+    val updateTime: String,
+    val userAssignId: Int
+)
